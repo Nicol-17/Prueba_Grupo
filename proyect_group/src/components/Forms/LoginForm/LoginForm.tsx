8c46b1c71802fb94a20f17b131c79a716116ebf4
Sergio Andres Gaona Benavides <sergioagaona@gmail.com>
2025-07-11 15:46:06 -0500

    Creation of a fake database using supabase for authentication

--- proyect_group/src/components/Forms/LoginForm/LoginForm.tsx
+++ proyect_group/src/components/Forms/LoginForm/LoginForm.tsx
@@ -4,44 +4,39 @@ import Button from "@/components/ui/Button/Button";
 import Input from "@/components/ui/Input/Input";
 import { useLoginForm } from "@/components/hooks/useLoginForm";
 
-
-export default function loginform() {
+export default function LoginForm() {
   const { formData, errors, handleChange, handleSubmit } = useLoginForm();
-  return (
-    <>
-      <form className="form-container" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username" >
-            Email
-          </label>
-          <Input
-            id="login-email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          {errors.email && <p className="">{errors.email}</p>}
-        </div>
 
-        <div>
-          <label htmlFor="password" className="">
-            Password
-          </label>
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <Button type={"button"} variant="primary">
-          Entrar
-        </Button>
-      </form>
-    </>
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Input
+          id="login-email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <Button type="submit" variant="primary">
+        Entrar
+      </Button>
+    </form>
   );
 }
