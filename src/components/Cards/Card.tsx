ff7b223020463fb8c38e389a03e7184448f3489f
Nicolle Dayanna Tovar Rojas <nicolle.tovar@globant.com>
2025-07-13 20:15:46 -0500

    add new component card html

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,45 @@
+import Image, {StaticImageData} from "next/image";
+
+
+type CardProps = {
+    name: string;
+    comment: string;
+    avatar: string ;
+    rating?: number;
+    id?: string;
+    
+    
+};
+
+function Card ({name, comment, avatar, rating = 5, id} :CardProps){
+    const stars = Array.from({length: 5}, (_, i) => (
+        <span key={i} className={i < rating ? "star filled" : "star"}>
+            ★
+        </span>
+    ))
+   
+    return(
+        <div id={id} className="">
+            <Image
+                src={avatar}
+                alt={`photo of ${name}`}
+                width={96}
+                height={96}
+                className=""
+            
+            />
+
+            
+            <h3 className="">{name}</h3>
+            <div className="">{stars}</div>
+            <p className="">{comment}</p>
+            
+        </div>
+
+
+
+)
+
+}
+
+export default Card;
